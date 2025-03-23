ca24c3ffa13342b1098dff6641e642a2c4c10307
val_l <contacttovalerie@gmail.com>
2025-03-23 09:52:46 -0400

    Stopped Audio lag with clones

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
-const textBox = document.querySelector('h1');
-const keySound = document.querySelector('.normal-key');
-const enterSound = document.querySelector('.enter');
-const backspaceSound = document.querySelector('.backspace');
+const textBox = document.querySelector("h1");
+const keySound = document.querySelector(".normal-key");
+const enterSound = document.querySelector(".enter");
+const backspaceSound = document.querySelector(".backspace");
 
-textBox.innerText = '';
+textBox.innerText = "";
 
-document.addEventListener('keydown', function(e) {
-    console.log(e.key);
-    keySound.pause();
+document.addEventListener("keydown", function (e) {
+  const playSound = function (sound) {
+    const soundClone = sound.cloneNode(true);
+    soundClone.play();
+  };
 
-    const span = document.createElement('span');
+  const span = document.createElement("span");
 
-    span.classList.add('animation');
+  span.classList.add("animation");
 
+  if (e.key === " ") {
+    span.innerText += "-";
+    playSound(keySound);
+  } else if (e.key === "Backspace") {
+    textBox.innerText = textBox.innerText.slice(0, -1);
+    playSound(backspaceSound);
+  } else if (e.key === "Enter") {
+    playSound(enterSound);
+  } else {
+    span.innerText = e.key;
 
-    if (e.key === ' ') {
-        span.innerText += '-';  
-        keySound.play();
-    } else if (e.key === 'Backspace') {
-        textBox.innerText = textBox.innerText.slice(0,-1);
-        backspaceSound.play();
-    } else if (e.key === 'Enter') {
-        enterSound.play();
-    } else {
-        span.innerText = e.key;
-        keySound.play();
-        if (!keySound.paused) {
-            keySound.currentTime = 0;
-            keySound.play();
-        } else {
-            keySound.play();  
-        }
-    }
+    playSound(keySound);
+  }
 
-    textBox.appendChild(span);
-
+  textBox.appendChild(span);
 });
-
